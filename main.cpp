cc799f9de767153210b02f54e9a08b285c17d648
Gustav Halvorsen <92439850+Guust207@users.noreply.github.com>
2022-04-27 15:47:54 +0200

    Finished setup
    Made a blinking program that compiles and runs on the microcontroller.
    Time for merge :D

--- main.cpp
+++ main.cpp
@@ -9,68 +9,14 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
+DigitalOut led1(LED1);
 
 int main()
 {
-    int ButtonOption;
-    bool clock;
-    bool alarm;
-    bool temp_hum;
-    bool weather;
+    while(true)
+    {
+        led1 = !led1;
 
-        while(true) {
-
-        if (ButtonOption == 5) {
-            ButtonOption = 1;
-        }
-
-
-        if (ButtonOption == 1) {
-            clock = true;
-            alarm = false;
-            temp_hum = false;
-            weather = false;
-        }
-        if (ButtonOption == 2) {
-            alarm = true;
-            temp_hum = false;
-            weather = false;
-            clock = false;
-        }
-        if (ButtonOption == 3) {
-            temp_hum = true;
-            alarm = false;
-            temp_hum = false;
-            weather = false;
-        }
-        if (ButtonOption == 4) {
-            weather = true;
-            temp_hum = false;
-            alarm = false;
-            temp_hum = false;
-        }
-
-
-
-        while(clock == true) {
-            clock_function();
-        }
-
-        while(clock == true) {
-            alarm_function();
-        }
-
-        while(clock == true) {
-            temp_hum_function();
-        }
-
-        while(clock == true) {
-            weaher_function();
-        }
-
-
-
+        ThisThread::sleep_for(BLINKING_RATE);
     }
-
-
 }
