8133f1f8e4b062742f197ff587bc6c2c6d5e27ac
Guust207 <92439850+Guust207@users.noreply.github.com>
2022-05-31 18:11:24 +0200

    Fixed some merge conflicts for merging

--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "BufferedSerial.h"
 #include "NetworkInterface.h"
 #include "SocketAddress.h"
