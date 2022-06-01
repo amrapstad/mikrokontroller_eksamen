e055d1b59876b6e45737fb680638881969b5b61f
Guust207 <92439850+Guust207@users.noreply.github.com>
2022-06-01 16:53:02 +0200

    Changed name of function

--- wifi.h
+++ wifi.h
@@ -18,7 +18,7 @@ nsapi_size_or_error_t read_response(Socket *socket, char *buffer,
                                     int buffer_length);
 
 const char *get_nsapi_error_string(nsapi_error_t err);
-void parse_json_data(char *input, int &unix_time);
+void parse_json_to_epoch(char *input, int &unix_time);
 void connect_to_BBC(NetworkInterface *network, struct NewsStrings *strings);
 void connect_to_WorldTime(NetworkInterface *network, int &unix_time);
 
